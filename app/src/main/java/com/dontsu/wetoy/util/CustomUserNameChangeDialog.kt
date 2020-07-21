6c4b05b972cbcf5dbaac759508d2444efc318d34
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-07-21 20:11:59 +0900

    커스텀다이얼로그
    AlertDialog로 만들면 keyboard 강제로 숨김 당하기 때문에 Dialog를 상속해서 구현해야함

--- app/src/main/java/com/dontsu/wetoy/util/CustomUserNameChangeDialog.kt
+++ app/src/main/java/com/dontsu/wetoy/util/CustomUserNameChangeDialog.kt
@@ -1,13 +1,20 @@
 package com.dontsu.wetoy.util
 
+import android.app.Dialog
 import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AlertDialog
 import com.dontsu.wetoy.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.custom_dialog_username_change.*
 
-class CustomUserNameChangeDialog(context: Context, customDialogInterface: CustomDialogInterface): AlertDialog(context) {
+class CustomUserNameChangeDialog(context: Context, customDialogInterface: CustomDialogInterface): Dialog(context) {
 
     private var customDialogInterface: CustomDialogInterface = customDialogInterface //인터페이스 연결
 
@@ -16,7 +23,10 @@ class CustomUserNameChangeDialog(context: Context, customDialogInterface: Custom
         setContentView(R.layout.custom_dialog_username_change)
 
         //배경 투명
-       /* window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) 투명 하니까 완전 안보임 걍 */
+        /*window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) 투명 하니까 완전 안보임 걍 */
+
+
+
 
         //변경버튼
         changeOkayBtn.setOnClickListener {
@@ -28,5 +38,17 @@ class CustomUserNameChangeDialog(context: Context, customDialogInterface: Custom
             customDialogInterface.onCancelClicked(this)
         }
 
+    }
+
+    private fun setTextChangeListener(et: TextInputEditText, til: TextInputLayout) {
+        et.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
     }
 }
--- app/src/main/java/com/dontsu/wetoy/viewmodel/UserInfoViewModel.kt
+++ app/src/main/java/com/dontsu/wetoy/viewmodel/UserInfoViewModel.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
